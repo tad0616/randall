ba41cf6513b268dec003ea00384d74e906257b84
Tad <tad0616@gmail.com>
2015-06-25 16:44:38 +0800

    新佈景釋出

---
+++ config2.php
@@ -0,0 +1,39 @@
+<?php
+$i                           = 0;
+$theme_config[$i]['name']    = "show_var";
+$theme_config[$i]['text']    = WMNHI_TF_SHOW_VAR;
+$theme_config[$i]['desc']    = WMNHI_TF_SHOW_VAR_DESC;
+$theme_config[$i]['type']    = "yesno";
+$theme_config[$i]['default'] = "0";
+
+//導覽列切換選項設定
+// $i++;
+// $theme_config[$i]['name']="navbar";
+// $theme_config[$i]['text']=WMNHI_NAVBAR;
+// $theme_config[$i]['desc']=WMNHI_NAVBAR_DESC;
+// $theme_config[$i]['type']="yesno";
+// $theme_config[$i]['default']="1";
+
+//頁尾底圖
+$i++;
+$theme_config[$i]['name']    = "footer_img";
+$theme_config[$i]['text']    = TF_FOOTER_IMG;
+$theme_config[$i]['desc']    = TF_FOOTER_IMG_DESC;
+$theme_config[$i]['type']    = "file";
+$theme_config[$i]['default'] = XOOPS_URL . "/themes/randall/images/2008112224010887.jpg";
+
+//網站名稱內容樣式設定
+$i++;
+$theme_config[$i]['name']    = "sitename_style";
+$theme_config[$i]['text']    = WMNHI_SITENAME_FONT_FAMILY;
+$theme_config[$i]['desc']    = WMNHI_SITENAME_FONT_FAMILY_DESC;
+$theme_config[$i]['type']    = "textarea";
+$theme_config[$i]['default'] = "";
+
+//網站口號內容樣式設定
+$i++;
+$theme_config[$i]['name']    = "slogan_style";
+$theme_config[$i]['text']    = WMNHI_SLOGAN_FONT_FAMILY;
+$theme_config[$i]['desc']    = WMNHI_SLOGAN_FONT_FAMILY_DESC;
+$theme_config[$i]['type']    = "textarea";
+$theme_config[$i]['default'] = "";
---
+++ language/tchinese/main.php
@@ -0,0 +1,47 @@
+<?php
+define("THEME_MODULE0", "主選單");
+define("THEME_MODULE1", "首頁");
+
+define("TF_USER_ID",  "請輸入帳號");
+define("TF_USER_PASS",  "請輸入密碼");
+define("TF_USER_S_ID",  "帳號：");
+define("TF_USER_S_PASS",  "密碼：");
+define("TF_USER_ENTER","登入");
+define("TF_USER_EXIT","登出");
+define("TF_USER_ADMIN","管理");
+define("TF_USER_BLOCK","區塊");
+define("TF_THEME_ADMIN","佈景");
+define("TF_THEME_DEBUG",  "開除錯");
+define("TF_THEME_UNDEBUG",  "關除錯");
+define("TF_USER_PROFILE","帳號");
+define("TF_USER_NOTICE","通知");
+define("TF_USER_NEWMSG","有新信");
+define("TF_USER_MSG","信件");
+define("TF_USER_WELCOME", "歡迎：");
+
+define("TF_SEARCH", "搜尋");
+define("TF_OK", "送出");
+define("TF_USER_REGIST",  "註冊");
+define("TF_USER_FORGET_PASS", "忘記密碼");
+
+define("TF_BLOCK_CONFIG", "設定本區塊");
+define("TF_SYSTEM_CONFIG",  "系統偏好設定");
+define("TF_SYSTEM_MODADM",  "線上安裝更新");
+define("TF_MODULE_CONFIG",  "模組管理區");
+define("TF_DL_TAD_THEMES",  "請安裝 Tad Themes 模組，可以讓您更進一步的調整佈景！");
+
+//以下為自訂語系
+ define("WMNHI_HELLO" , "您好!");
+ define("WMNHI_USER_LOGO" , "從佈景管理的後台變換自己的logo");
+ define("WMNHI_SLIDE_PICTURE" , "從佈景管理的後台設定畫面，上傳自己喜歡圖片");
+ define("WMNHI_TF_SHOW_VAR" , "顯示佈景變數?");
+ define("WMNHI_TF_SHOW_VAR_DESC" , "是否顯示佈景變數?");
+ // define("WMNHI_NAVBAR" , "導覽列選項：tad樣版→是  wnmhi樣版→否");
+ // define("WMNHI_NAVBAR_DESC" , "tad為預設導覽列樣版，使用tad樣版者可由導覽工具調整相關設定");
+ define("WMNHI_SITENAME_FONT_FAMILY","網站名稱內容樣式設定");
+ define("WMNHI_SITENAME_FONT_FAMILY_DESC", "可使用CSS語法控制網站名稱內容的樣式");
+ define("WMNHI_SLOGAN_FONT_FAMILY","網站口號內容樣式設定");
+ define("WMNHI_SLOGAN_FONT_FAMILY_DESC", "可使用CSS語法控制網站口號內容的樣式");
+ define("TF_FOOTER_IMG", "頁尾底圖");
+ define("TF_FOOTER_IMG_DESC", "可自行上傳想要的底圖");
+?>
